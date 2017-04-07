dfa669e8557eaf3b130982fde1d64cf947c051d3
xiaowenhao <rivers@172-13-2-145.lightspeed.mssnks.sbcglobal.net>
2017-04-07 16:25:01 +0800

    sound of the voice

---
+++ flatten.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+n,m = map(int,input().split())
+data = []
+for _ in range(n):
+    data.append(input().split())
+data = np.array(data, int)
+print (np.transpose(data))
+print (data.flatten())
+#flatten the array to one dimension
